1d3e014204965d2d98fad5f74f55b870416f6541
DeianMarioRus <deianrus04@gmail.com>
2023-01-12 12:16:09 +0100

    Primer commit

---
+++ Compania.java
@@ -0,0 +1,13 @@
+public class Compania{
+   
+    public String nombreCompania;
+    public int codigoCompania;
+    
+    public Compania(String nombreDeCompania, int codigoDeCompania)
+    {
+        nombreCompania = nombreDeCompania;
+        codigoCompania = codigoDeCompania;
+    }
+
+    
+}
