aecbefc067f8871b82aa16c973c2c25c20385028
DeianMarioRus <deianrus04@gmail.com>
2023-01-13 09:17:16 +0100

    Quinto commit

--- Arma.java
+++ Arma.java
@@ -1,9 +1,13 @@
 
 
-public class Arma
-{
-   private int capacidadCargador;
-   private String calibre;
-   private Compania companiaQueFabrica;
+public class Arma{
+    private int capacidadCargador;
+    private String calibre;
+    private Compania companiaQueFabrica;
     
+    public Arma(int capacidadDe,String calibreDe )
+    {
+        capacidadCargador = capacidadDe;
+        calibre = calibreDe;
+    }
 }
