c833039123309b6e51e85d9fda23a6d9faa79689
Alejandro Fuente Garcia <afuengar2001@gmail.com>
2023-01-13 19:17:19 +0100

    Sexto commit

--- Arma.java
+++ Arma.java
@@ -5,9 +5,34 @@ public class Arma{
     private String calibre;
     private Compania companiaQueFabrica;
     
-    public Arma(int capacidadDe,String calibreDe )
+    public Arma(int capacidadDe,String calibreDe, Compania companiaDelArma)
     {
         capacidadCargador = capacidadDe;
         calibre = calibreDe;
+        companiaQueFabrica = companiaDelArma;
+    }
+    
+    public void setCambiarCalibre(String nuevoCalibre){
+        calibre = nuevoCalibre;
+    }
+    
+    public void setCambiarCapacidadCargador(int nuevaCapacidadCargador){
+        capacidadCargador = nuevaCapacidadCargador;
+    }
+    
+    public void setNuevaCompania(Compania nuevaCompaniaQueFabrica){
+        companiaQueFabrica = nuevaCompaniaQueFabrica;
+    }
+    
+    public String getCalibre(){
+        return calibre;
+    }
+    
+    public int getCapacidadCargador(){
+        return capacidadCargador;
+    }
+    
+    public Compania getCompaniaQueFabrica(){
+        return companiaQueFabrica;
     }
 }
